a454e20e678ad8b38cd744e334d3b5b3e884e6c8
Kateryna911 <ptica.katia@gmail.com>
2021-04-12 23:33:57 +0300

    add task 14

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,27 +1,36 @@
-// const ListNode = require('../extensions/list-node');
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- *
- */
+const ListNode = require('../extensions/list-node');
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.head = null;
+    this.length = 0;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    return this.length;
+  }
+
+  enqueue(element) {
+    const node = new ListNode(element);
+
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.length++;
+    return node.value;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    const removed = this.head;
+    this.head = this.head.next;
+    this.length--;
+    return removed.value;
   }
 }
 
