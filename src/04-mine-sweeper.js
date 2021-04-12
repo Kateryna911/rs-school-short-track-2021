75b2cf47808240be3aa6ece1e5bff33cc95e9e53
Kateryna911 <ptica.katia@gmail.com>
2021-04-12 22:50:19 +0300

    add js task 4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -1,28 +1,14 @@
-/**
- * In the popular Minesweeper game you have a board with some mines and those cells
- * that don't contain a mine have a number in it that indicates the total number of mines
- * in the neighboring cells. Starting off with some arrangement of mines
- * we want to create a Minesweeper game setup.
- *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
- *
- * @example
- * matrix = [
- *  [true, false, false],
- *  [false, true, false],
- *  [false, false, false]
- * ]
- *
- * The result should be following:
- * [
- *  [1, 2, 1],
- *  [2, 1, 1],
- *  [1, 1, 1]
- * ]
- */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+const matrix = [
+  [true, false, false],
+  [false, true, false],
+  [false, false, false],
+];
+// eslint-disable-next-line no-shadow
+function minesweeper(matrix) {
+  // eslint-disable-next-line max-len
+  return matrix.map((l, i) => l.map((e, j) => matrix.slice(Math.max(0, i - 1), i + 2).map((f) => f.slice(Math.max(0, j - 1), j + 2).reduce((a, b) => a + b)).reduce((a, b) => a + b) - e));
 }
+// eslint-disable-next-line no-console
+console.log(minesweeper(matrix));
 
 module.exports = minesweeper;
